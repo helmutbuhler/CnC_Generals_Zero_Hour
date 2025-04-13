172694509ecdca9028e33761c1b7408eaa293e6f
Helmut Buhler <buhler@8gadgetpack.net>
2025-04-13 11:16:09 +0200

    Fix messy code and potential null pointer dereference in RecorderClass::appendNextCommand()

--- GeneralsMD/Code/GameEngine/Source/Common/Recorder.cpp
+++ GeneralsMD/Code/GameEngine/Source/Common/Recorder.cpp
@@ -1296,16 +1296,6 @@ void RecorderClass::appendNextCommand() {
 	}
 
 	GameMessage *msg = newInstance(GameMessage)(type);
-	if (type == GameMessage::MSG_BEGIN_NETWORK_MESSAGES || type == GameMessage::MSG_CLEAR_GAME_DATA)
-	{
-	}
-	else
-	{
-		if (!m_doingAnalysis)
-		{
-			TheCommandList->appendMessage(msg);
-		}
-	}
 
 #ifdef DEBUG_LOGGING
 	AsciiString commandName = msg->getCommandAsAsciiString();
@@ -1377,13 +1367,11 @@ void RecorderClass::appendNextCommand() {
 		}
 	}
 
-	if (type == GameMessage::MSG_CLEAR_GAME_DATA || type == GameMessage::MSG_BEGIN_NETWORK_MESSAGES)
+	if (type != GameMessage::MSG_BEGIN_NETWORK_MESSAGES && type != GameMessage::MSG_CLEAR_GAME_DATA && !m_doingAnalysis)
 	{
-		msg->deleteInstance();
-		msg = NULL;
+		TheCommandList->appendMessage(msg);
 	}
-
-	if (m_doingAnalysis)
+	else
 	{
 		msg->deleteInstance();
 		msg = NULL;
