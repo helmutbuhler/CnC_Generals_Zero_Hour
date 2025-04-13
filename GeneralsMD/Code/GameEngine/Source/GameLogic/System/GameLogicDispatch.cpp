18605353c4b0c13b8eac106eb13568aeeb67b26c
Helmut Buhler <buhler@8gadgetpack.net>
2025-04-13 17:16:27 +0200

    Fix headless when shutting down the engine.

--- GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogicDispatch.cpp
+++ GeneralsMD/Code/GameEngine/Source/GameLogic/System/GameLogicDispatch.cpp
@@ -268,7 +268,7 @@ void GameLogic::clearGameData( Bool showScoreScreen )
 	TheScriptActions->closeWindows(FALSE); // Close victory or defeat windows.
 
 	Bool shellGame = FALSE;
-	if ((!isInShellGame() || !isInGame()) && showScoreScreen)
+	if ((!isInShellGame() || !isInGame()) && showScoreScreen && !TheGlobalData->m_headless)
 	{
 		shellGame = TRUE;
 		TheTransitionHandler->setGroup("FadeWholeScreen");
@@ -302,7 +302,8 @@ void GameLogic::clearGameData( Bool showScoreScreen )
 	HideControlBar();
 	closeWindows();
 
-	TheMouse->setVisibility(TRUE);
+	if (TheMouse != NULL)
+		TheMouse->setVisibility(TRUE);
 
 	if(m_background)
 	{
